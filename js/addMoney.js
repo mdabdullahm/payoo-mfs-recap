8594ef8337b5c1956a0ca449f10679b5bcb316d1
mdabdullahm <you@example.com>
2024-09-27 20:47:59 +0600

    practiced add money function

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,8 +2,20 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
 
     const addMoney = document.getElementById('input-add-money').value;
-
+    const addMoneyNumber = parseFloat(addMoney);
     const piNumber = document.getElementById('input-pin-number').value;
 
     console.log(addMoney, piNumber);
+    // wrong way to verify pin
+    if(piNumber === '1234'){
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+
+        const newBalance = balanceNumber + addMoneyNumber;
+        
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money.')
+    }
 })
